845caa774b4b243451ae64bb0eb9fb9b0fe6220b
Fabio Cibecchini <fabio.cibecchini@gmail.com>
2016-06-07 12:28:09 +0200

    Updated menu style

--- content/themes/default/views/menu.php
+++ content/themes/default/views/menu.php
@@ -6,18 +6,17 @@ if (! defined ( 'BASEPATH' ))
 <div class="list">
 	<div class="title">
 		<a href="<?php echo site_url($link) ?>"><?php echo $title; ?></a>
-		<?php 
-		echo form_open("search_".$param."/");
-		echo form_input(array('name' => 'search', 'placeholder' => _('To search, type and hit enter'), 'id' => 'searchbox', 'class' => 'fright'));
-		echo form_close();
-		?>
 	</div>
-	<?php
+	<?php 
+	echo form_open("search_".$param."/");
+	echo form_input(array('name' => 'search', 'placeholder' => _('To search, type and hit enter'), 'id' => 'menusearchbox', 'class' => 'menu'));
+	echo form_close();
+
 	$old = '';
 	foreach ( $comics as $key => $comic ) {
 		$current = $comic->$param_stub;
 		if ($current !== $old)
-			echo '<div class="element"><div class="title"><a href="' . base_url () . $param . '/'. $comic->$param_stub .'">' . $comic->$param . '</a></div></div>';
+			echo '<a class="attribute-row" href="' . base_url () . $param . '/'. $comic->$param_stub .'">' . $comic->$param . '</a>';
 		$old = $current;
 	}
 	echo prevnext($link.'/', $comics);
